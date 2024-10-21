ac4e01aeb9f13cc352a2dcb3eb28c8c54ca7c71f
Daniela <danamattl@yahoo.com>
2024-10-21 13:05:52 +0200

    page home

--- src/app/App.js
+++ src/app/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from '../pages/Home';
+import Home from '../pages/Home/Home';
 import Login from '../pages/Login';
 import User from '../pages/User';
-import Error from '../pages/Error';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer /Footer'
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/Login' element={<Login />}/>
         <Route path='/User' element={<User />}/>
-        <Route path='*' element={<Error />}/>
       </Routes>
+      <Footer />
     </Router>
   )
 }
